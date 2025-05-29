42f1783796127c7e5a7bb3cc52d58675fc85a6a2
root <root@DESKTOP-39LDDVO>
2025-05-29 20:00:21 +0530

    made changes in app.js

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/public/index.html'));
 });
 
 // Your API routes here (if any)
